87f6793fe942b6a12de36145231af6f6195d1739
Rebecca Anne Sealfon <ras2198@columbia.edu>
2014-06-19 21:42:27 -0400

    Create empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Recruittech::Application.config.secret_key_base = '0f3ed328ff6e9d630615b89d1ede886781e8c5c7c63741db4c36a191c51186b42a6556ac1baa24df3dc8d00f1ce14aed90096c0d49179fcfba8fa8180029f7e7'
